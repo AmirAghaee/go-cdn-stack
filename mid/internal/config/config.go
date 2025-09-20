93a73cb9e198801803afc8a3a8eb9a66ee65da32
aghaee <a.aghaei@asiatech.cloud>
2025-09-20 14:19:53 +0330

    implement basic of mid service

---
+++ mid/internal/config/config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	GinMode         string
+	Port            string
+	ControlPanelUrl string
+	NatsUrl         string
+}
+
+func Load() *Config {
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("No .env file found, using defaults")
+	}
+
+	config := &Config{
+		GinMode:         "debug",
+		ControlPanelUrl: "http://localhost:9000",
+		Port:            "8080",
+		NatsUrl:         "nats://localhost:4222",
+	}
+
+	// control panel url
+	if ControlPanelUrl := os.Getenv("CONTROL_PANEL_URL"); ControlPanelUrl != "" {
+		config.ControlPanelUrl = ControlPanelUrl
+	}
+
+	// Load gin mode
+	if ginMode := os.Getenv("APP_MODE"); ginMode != "" {
+		config.GinMode = ginMode
+	}
+
+	// Load app port
+	if Port := os.Getenv("PORT"); Port != "" {
+		config.Port = Port
+	}
+
+	// Load app port
+	if NatsUrl := os.Getenv("NATS_URL"); NatsUrl != "" {
+		config.NatsUrl = NatsUrl
+	}
+
+	return config
+}
--- mid/main.go
+++ mid/main.go
@@ -1,1 +1,34 @@
-package mid
+package main
+
+import (
+	"fmt"
+	"log"
+	"mid/internal/config"
+	"mid/internal/messaging"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	cfg := config.Load()
+
+	// setup NATS publisher
+	natsBroker, err := messaging.NewNatsBroker(cfg.NatsUrl)
+	if err != nil {
+		log.Fatalf("messaging connect: %v", err)
+	}
+
+	err = natsBroker.Subscribe("cdn.snapshot", func(msg string) {
+		fmt.Println("📩 Received message:", msg)
+	})
+	if err != nil {
+		log.Fatalf("subscribe failed: %v", err)
+	}
+
+	r := gin.Default()
+	//	h := http.NewHTTPHandler(cdnService, userService, natsPublisher)
+	//h.Register(r)
+
+	fmt.Printf("Server running on :%s\n", cfg.Port)
+	_ = r.Run(":" + cfg.Port)
+}
